1a0afaa519d426f7e61d867a926f97b9f606418d
VVILAS\vvilas <vitor.vboas@gmail.com>
2017-05-22 21:53:44 -0300

    Area publica quase pronta (Login, catalogo, cadastroUsuario), fantando só internacionalizar, e "link" do CSS

--- WebContent/resources/js/js.js
+++ WebContent/resources/js/js.js
@@ -1,6 +1,8 @@
 $(document).ready(function () { 
         $("#idCPF").mask('000.000.000-00');
         $("#idCNPJ").mask('00.000.000/0000-00');
+        $("#idCEP").mask('00000-000');
+
     });
 
 function filtro() {
