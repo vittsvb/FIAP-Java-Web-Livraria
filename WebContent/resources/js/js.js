f73bef659b053927ac288a00e30c33d244e31449
VVILAS\vvilas <vitor.vboas@gmail.com>
2017-05-23 10:27:44 -0300

    bug fix

--- WebContent/resources/js/js.js
+++ WebContent/resources/js/js.js
@@ -2,9 +2,7 @@ $(document).ready(function () {
         $("#idCPF").mask('000.000.000-00');
         $("#idCNPJ").mask('00.000.000/0000-00');
         $("#idCEP").mask('00000-000');
-        $("#idIdade").mask('000');
-
-
+        $("#idIdade").mask('00');
     });
 
 function filtro() {
